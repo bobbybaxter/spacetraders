de168cfce0a23f0878bc48169d8a84c39c1d2517
Bob Baxter <bob.e.baxter@gmail.com>
2023-06-07 15:17:58 -0500

    updated ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: 'spacetraders-api',
       script: './dist/index.js',
+      node_args: '--experimental-loader',
       env: {
         NODE_ENV: 'production',
         TS_NODE_BASEURL: './dist',
